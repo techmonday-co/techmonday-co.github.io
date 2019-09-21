759b0cd9e8e5f8d3cfb7a14df30e4ba454d292b2
Roonglit Chareonsupkul <roonglit@gmail.com>
2019-09-21 21:24:06 +0700

    integrate sass

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,3 @@
-// THIRD PARTIES
-import "./node_modules/bootstrap/dist/css/bootstrap.min.css"
-
 // GLOBAL
 import "./src/styles/fonts.css"
 import "./src/styles/global.css"
@@ -8,3 +5,5 @@ import "./src/styles/global.css"
 // COMPONENTS
 import "./src/styles/header.css"
 import "./src/styles/footer.css"
+
+import "./src/styles/application.scss"
