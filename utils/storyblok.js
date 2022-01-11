274404866a82f296fbda72ae33122fd308981611
Roonglit Chareonsupkul <roonglit@gmail.com>
2022-01-11 20:59:30 +0700

    apply environment variable for storyblok access token

--- utils/storyblok.js
+++ utils/storyblok.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import StoryblokClient from "storyblok-js-client";
 
 const Storyblok = new StoryblokClient({
-  accessToken: "YOUR_PREVIEW_TOKEN",
+  accessToken: process.env.STORYBLOK_ACCESS_TOKEN,
   cache: {
     clear: "auto",
     type: "memory",
